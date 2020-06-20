e45522ea88bca91144f5343258f9c80ac626a39a
ali kolukirik <aakolukirik@gmail.com>
2020-06-20 14:01:23 +0300

    Add files via upload

---
+++ soru00.java
@@ -0,0 +1,18 @@
+package algoritma;
+
+public class soru00 {
+
+	public static void main(String[] args) {
+		
+		/*Java dilinde çýktý almak için System.out.print.. komutunu kullanmamýz gerekiyor
+		 * ayný þekilde System.out.println kullanmamýz durumunda ifademiz bir alt satýrdan
+		 * baþlayacaktýr.
+		 * 
+		 * Java ortamýnda string (harf,kelime )ifadeleri yazdýrmamýz için onlarý týrnak içine almamýz gerekmektedir.
+		 */
+		
+		System.out.println("merhaba");
+
+	}
+
+}
