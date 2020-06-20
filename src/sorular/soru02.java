c074a41151167a3ec654e9b823c1f2a45d10960a
ali k <48579587+akolukirik@users.noreply.github.com>
2020-06-20 14:35:04 +0300

    Sorular

---
+++ src/sorular/soru02.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sorular;
+
+/**
+ *
+ * @author aakol
+ */
+public class soru02 {
+    public static void main(String[] args) {
+        
+        System.out.println("deneme 222");
+    }
+    
+}
